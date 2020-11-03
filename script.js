cb18ca9980266d48d8762ffaef7f65ed9d7dcbd1
Peter Boyle <boyle.peter614@gmail.com>
2020-11-03 01:00:44 -0500

    old js replaced with new js

--- script.js
+++ script.js
@@ -14,24 +14,36 @@ function writePassword() {
 
 var passLen = parseInt(prompt("How many characters long should your password be?"))
 
-//*var alphabet1 = "abcdejghijklmnopqrstuvwxyz"
-//var alphabet2 = "abcdejghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-//var alphabet3 = "abcdejghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-//var alphabet4 = "abcdejghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
+var lower = "abcdejghijklmnopqrstuvwxyz";
+var upper = "abcdejghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "abcdejghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+var special = "abcdejghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+var letters = "";
 
-//if confirm("Would you like to use upper, lower case, numbers, and special characters?"){
-//  charset=alphabet4
-//}
-//else {confirm("Would you like to use upper, lower case, and numbers?"){
-//  charset=alphabet3}
-//}
-//else {confirm("Would you like to use upper and lower case?"){
-//  charset=alphabet2
-//}}
-//else {charset=alphabet1}
+var checkSpecial = confirm("Would you like to use upper, lower case, numbers, and special characters?");
+
+var checkNumbers = confirm("Would you like to use upper, lower case, and numbers?")
+
+var checkUpper = confirm("Would you like to use upper and lower case?")
+
+var checkLower = confirm("Would you like to use lower case letters?")
+
+  if (checkLower) {
+   letters += lower;
+  }
+  if (checkUpper) {
+  letters += upper;
+  }
+  if (checkNumbers) {
+  letters += numbers;
+  }
+  if (checkSpecial) {
+  letters += special;
+  }
+       
 
 
-function generatePassword(length=passLen, charset="abcdejghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()") {
+function generatePassword(length=passLen, charset=letters) {
     return new Array(length)
       .fill(null)
       .map(()=> charset.charAt(Math.floor(Math.random() * charset.length)))
